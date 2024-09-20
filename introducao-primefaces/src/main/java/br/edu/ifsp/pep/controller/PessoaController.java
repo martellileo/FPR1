840226ef24dfc3b9ea67f81ca3598535daa797cf
Léo Martelli <l.martelli@aluno.ifsp.edu.br>
2024-09-20 09:36:45 -0300

    alterações

---
+++ introducao-primefaces/src/main/java/br/edu/ifsp/pep/controller/PessoaController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.controller;
+
+import jakarta.inject.Named;
+
+/**
+ *
+ * @author aluno
+ */
+@Named
+public class PessoaController {
+    public void exibir(){
+        System.out.println("Método exibir");
+    }
+}
